c0e00c2600f8f359409e1f89968f7e0ade1d56ed
chengyiwei <1485868106@qq.com>
2019-04-21 02:05:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
+/*******************************************************
+* 文件名: main.cpp
+* 描  述: noip_help的主程序
+* 日  期: 2019-4-21
+* 官方QQ: 1485868106
+* 版  权: (c) 2019 xinghua middle school CYW。保留所有权利。
+********************************************************/
+
 #include <iostream>
-#include "lib.h"
-using namespace std;
+#include <windows.h>
+#include <cstdio>
+
+//#include "lib.h"
 
 int main()
 {
-    //这是utf-8
-    print();
+  //  SetTitle((char *)"noip_help");
+    printf("你好");
+    system("pause");
     return 0;
 }
