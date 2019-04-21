0777c32ebae278e490f0fff00d0be9ef4a972a16
chengyiwei <1485868106@qq.com>
2019-04-21 14:18:34 +0800

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -81,3 +81,24 @@ void SetColorXY(const int& Le_x,const int& Ri_x,const int& Up_y,const int& Dn_y,
 	}
 	return ;
 }
+
+/* 获取键盘按键 */
+int GetKey()
+{
+    HANDLE handle;          //当前窗口句柄
+    handle = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD Read;
+    INPUT_RECORD Event;
+    //得到控制台的输入
+    ReadConsoleInput(handle, &Event, 1, &Read);
+    //如果输入事件是一个键盘事件，就进行判断，返回按下键的虚拟键码
+    if(Event.EventType == KEY_EVENT)
+    {
+        if(Event.Event.KeyEvent.bKeyDown)//返回值为true，表示有键被按下
+        {
+             return Event.Event.KeyEvent.wVirtualKeyCode;//返回按下键的虚拟键码
+        }
+    }
+    return 0;
+}
+
