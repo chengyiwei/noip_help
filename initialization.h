178768432932841fb75aa1d0a35ec719a9d20615
chengyiwei <1485868106@qq.com>
2019-04-23 23:24:25 +0800

    Create initialization.h

---
+++ initialization.h
@@ -0,0 +1,9 @@
+#ifndef INITIALIZATION_H_INCLUDED
+#define INITIALIZATION_H_INCLUDED
+#include <fstream>
+#include <iostream>
+#include "lib.h"
+
+int activation();
+
+#endif // INITIALIZATION_H_INCLUDED
