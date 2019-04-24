bc7ed35373942259e0228f4400b945767eb5c3a9
chengyiwei <1485868106@qq.com>
2019-04-25 00:47:39 +0800

    Update initialization.h

--- initialization.h
+++ initialization.h
@@ -1,3 +1,10 @@
+/*******************************************************
+* 文件名: initialization.h
+* 描  述: noip_help的初始化程序
+* 日  期: 2019-4-21
+* 官方QQ: 1485868106
+* 版  权: (c) 2019 xinghua middle school CYW。保留所有权利。
+********************************************************/
 #ifndef INITIALIZATION_H_INCLUDED
 #define INITIALIZATION_H_INCLUDED
 #include <fstream>
