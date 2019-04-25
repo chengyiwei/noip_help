ee32e4b6ec057aacfebc90160a585c209d167339
chengyiwei <1485868106@qq.com>
2019-04-26 00:06:30 +0800

    Update core.h

--- core.h
+++ core.h
@@ -1,6 +1,15 @@
+/*******************************************************
+* 文件名: core.h
+* 描  述: noip_help的核心运行程序
+* 日  期: 2019-4-24
+* 官方QQ: 1485868106
+* 版  权: (c) 2019 xinghua middle school CYW。保留所有权利。
+********************************************************/
 #ifndef CORE_H_INCLUDED
 #define CORE_H_INCLUDED
 
+#include "initialization.h"
 
+void run();
 
 #endif // CORE_H_INCLUDED
